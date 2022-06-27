fd07938a972544ead0b60d4d0921038002132c7f
SuzBarnes <SuzBarnes@github.com>
2022-06-27 16:41:51 +0100

    DRY coding, added beforeEach()

--- __tests__/Itinerary.test.js
+++ __tests__/Itinerary.test.js
@@ -3,15 +3,22 @@ const Ship = require('../src/Ship.js');
 const Itinerary = require ('../src/Itinerary.js')
 
 describe ('Itinerary', () => {
-    it ('can be instantiated', () => {
-expect (new Itinerary()).toBeInstanceOf(Object);
-    });
-    it('has a ports property', () => {
-        const dover = new Port ('Dover');
-        const calais = new Port ('Calais');
+    describe ('with ships and ports', () => {
+let dover;
+let calais;
+let itinerary;
 
-        const itinerary = new Itinerary ([dover, calais]);
-
-expect (itinerary.ports).toEqual([dover, calais]);
-    });
+beforeEach(() => {
+dover = new Port ('Dover');
+calais = new Port ('Calais');
+itinerary = new Itinerary([dover, calais]);
 });
+        it ('can be instantiated', () => {
+    expect (new Itinerary()).toBeInstanceOf(Object);
+        });
+        it('has a ports property', () => {
+    
+    expect (itinerary.ports).toEqual([dover, calais]);
+        });
+    });
+    });
--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -2,39 +2,47 @@ const Port = require('../src/Port.js');
 const Ship = require('../src/Ship.js');
 
 describe ('Port', () => {
+  describe('with ships and an itinerary', () => {
+    let calais;
+    let dover;
+    let ship;
+    let titanic;
+    let queenMary;
+
+    beforeEach (() => {
+      calais = new Port('Calais');
+      dover = new Port ('Dover');
+      ship = {}
+      titanic = {};
+      queenMary ={};
+  });
+
+
     it ('can be instantiated', () => {
       expect (new Port()).toBeInstanceOf(Object);
     });
 
-});
-
  it('sets the name property', ()=> {
-        const port = new Port ('Calais')
 
-        expect (port.name).toEqual('Calais');
+        expect (calais.name).toEqual('Calais');
         
 });
 describe ('addShip', () =>{
 it('adds the ship to the ports ships', () => {
-        const port = new Port ('Dover');
-        const ship ={};
-        
-        port.addShip(ship);
-      expect(port.ships).toContain(ship);
-});
-
+      
+        dover.addShip(ship);
+      expect(dover.ships).toContain(ship);
 });
 
 describe ('removeShip', () => {
 it ('removes the ship from the port ships', () => {
-const port = new Port ('Dover');
-const titanic = {};
-const queenMary = {};
+dover.addShip(titanic);
+dover.addShip(queenMary);
+dover.removeShip(queenMary);
 
-port.addShip(titanic);
-port.addShip(queenMary);
-port.removeShip(queenMary);
-
-expect (port.ships).toEqual([titanic]);
+expect (dover.ships).toEqual([titanic]);
 });
 });
+});
+});
+  });
