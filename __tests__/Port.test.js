9925c040fd23c040198319666dd6196f84c98c38
SuzBarnes <SuzBarnes@github.com>
2022-06-27 11:53:38 +0100

    updated ports and ship

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -10,8 +10,31 @@ describe ('Port', () => {
 
  it('sets the name property', ()=> {
         const port = new Port ('Calais')
-        
+
         expect (port.name).toEqual('Calais');
         
+});
+describe ('addShip', () =>{
+it('adds the ship to the ports ships', () => {
+        const port = new Port ('Dover');
+        const ship ={};
+        
+        port.addShip(ship);
+      expect(port.ships).toContain(ship);
+});
 
+});
+
+describe ('removeShip', () => {
+it ('removes the ship from the port ships', () => {
+const port = new Port ('Dover');
+const titanic = {};
+const queenMary = {};
+
+port.addShip(titanic);
+port.addShip(queenMary);
+port.removeShip(queenMary);
+
+expect (port.ships).toEqual([titanic]);
+});
 });
--- src/Port.js
+++ src/Port.js
@@ -2,7 +2,24 @@
 class Port{
     constructor (name){
         this.name = name;
-};
+        this.ships = [];
 };
 
+
+addShip(ship){
+    
+    this.ships.push(ship);
+
+};
+
+removeShip(ship){
+  
+    const indexOfShipToRemove = this.ships.indexOf(ship);
+        if (indexOfShipToRemove > -1){
+            this.ships.splice(indexOfShipToRemove, 1);
+        };
+        return this.ships;
+
+}
+};
 module.exports = (Port);
